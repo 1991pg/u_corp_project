f642e6af9fc4ff0650a2f23aebb431f5f2d3c365
1991pg <pgroenewout@gmail.com>
2017-05-04 20:26:56 +0200

    third attempt

--- u_corp/ViewController.swift
+++ u_corp/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         
         // Do any additional setup after loading the view, typically from a nib.
         
+        print("hellow world this is adjusted ")
     }
 
     override func didReceiveMemoryWarning() {
