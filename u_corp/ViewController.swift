5d4a221cb9ef8d3824152f90e8dba8f785110430
1991pg <pgroenewout@gmail.com>
2017-05-04 20:24:57 +0200

    second attempt

--- u_corp/ViewController.swift
+++ u_corp/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
         
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("hellow world")
     }
 
     override func didReceiveMemoryWarning() {
